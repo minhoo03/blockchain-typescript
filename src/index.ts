bcfda84efffff7a5c3a9e335316dc9fac7a9241a
minhoo03 <minhoo0333@gmail.com>
2021-05-13 22:35:41 +0900

    02-1. interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hi!')
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name: "minhoo",
+    age: 19,
+    gender: "male"
+}
+
+const sayHi = (person:Human): string => {
+    return `Hello ${person.name}, you are ${person.age}! You are a ${person.gender}`
+}
+
+console.log(sayHi(person))
+
